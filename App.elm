150ce471bdcb4960437726a6cb3dea9dd61427e1
Gertjan Reynaert <gertjan@typework.com>
2016-05-24 16:33:54 +0200

    temp(WIP): cleanup

--- App.elm
+++ App.elm
@@ -2,22 +2,29 @@ import Hex exposing (darken, lighten)
 
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (style)
+import String
+
 
 square color =
   let
     styles = style
-      [ ("width", "40px")
-      , ("height", "40px")
+      [ ("width", "60px")
+      , ("height", "60px")
+      , ("margin", "20px")
+      , ("padding", "20px")
+      , ("border-radius", "4px")
+      , ("color", "white")
+      , ("font-family", "sans-serif")
+      , ("font-weight", "")
       , ("background", color)
       ]
   in
-    div [ styles ] [ text color]
+    div [ styles ] [ text (String.toUpper color)]
 
 
 main =
   div []
-    [ (square (darken "#00f" 0 ))
-    , (square (darken "#00f" 30))
-    , (square (lighten "#f00" 0))
-    , (square (lighten "#f00" 30))
+    [ (square (lighten "#f00" 30))
+    , (square "#f00")
+    , (square (darken "#f00" 30))
     ]
